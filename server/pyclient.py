038cb409b0a4d6ebe40237ccdef94e71b3144e38
DeliriumNocturnum <jake@schwartzpub.com>
2013-11-23 21:37:04 -0600

    test push

--- server/pyclient.py
+++ server/pyclient.py
@@ -1,14 +1,17 @@
+#!/usr/bin/env python
+
 import socket
 from threading import Thread
 import SocketServer
 import sys
 import os
 import signal
+
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('sh.schwartzpub.com', 4898)
+server_address = ('', port)
 print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 sock.settimeout(10)
@@ -30,30 +33,22 @@ def recv():
 				print dat
 				dat = ''
 		else: dat += data
-				
-
-#def recv():
-#	while True:
-#		data = sock.recv(8192)
-#		if data == 'CLRSCRN_FULL':
-#			os.system('clear')
-#			continue
-#		if not data:
-#			sys.exit(0)
-#		print data
 
 def senddata():
-	while True:		
-		data = raw_input("")
-		if not data: 
-			continue
-		elif data == '!quit':
-			sock.sendall(data)
-			break
-		else:
-			sock.sendall(data)
-			sock.settimeout(None)
-			continue
+	try:
+		while True:		
+			data = raw_input("")
+			if not data: 
+				continue
+			elif data == '!quit':
+				sock.sendall(data)
+				break
+			else:
+				sock.sendall(data)
+				sock.settimeout(None)
+				continue
+	except socket.timeout:
+		sys.exit(0)
 
 def signal_handler(signal, frame):
 	try:
